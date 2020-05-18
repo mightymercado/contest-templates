d1f63aadd0fc29e31e928aa8a586a8265f6f54bf
Ken Mercado <mightymercado@gmail.com>
2020-05-18 13:33:15 +0800

    Create ternary_search.cpp

---
+++ ternary_search.cpp
@@ -0,0 +1,16 @@
+double t_search(function<double(double)> f, double l, double r) {
+    for (int i = 0; i < 100; i++) {
+			double m1 = (l * 2 + r) / 3;
+			double m2 = (l + 2 * r) / 3;
+			f(m1) < f(m2) ? r = m2 : l = m1; // min <, max > 
+    }
+    return f(l);
+}
+
+int t_search(function<long long(int)> f, int l, int r) {
+  for (--l; r - l > 1; ) {
+    int m = (r + l) >> 1;
+    f(m) < f(m + 1) ? r = m : l = m; // min <, max >
+  }
+  return l + 1; 
+}
